9e7358aa40112996cedb7038c1d326e7fd70fe87
Pham Bao Phuc <114333943+phambaophuc@users.noreply.github.com>
2024-03-20 23:55:25 +0700

    Update ProductServiceApplication.java

--- src/main/java/org/pbp/productservice/ProductServiceApplication.java
+++ src/main/java/org/pbp/productservice/ProductServiceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @OpenAPIDefinition(info =
-@Info(title = "Product API", version = "${springdoc.version}", description = "Documentation Product API v1.0")
+@Info(title = "Product API", version = "${springdoc.version}", description = "Documentation Product API v1.0"))
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
